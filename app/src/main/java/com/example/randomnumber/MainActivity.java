d2e5b884ed4d5e260bf54d9316a7875bc957e56a
Christina-Kontogiorgi <103931174+Christina-Kontogiorgi@users.noreply.github.com>
2022-05-09 15:44:29 +0300

    Learn about Lists and Random

--- app/src/main/java/com/example/randomnumber/MainActivity.java
+++ app/src/main/java/com/example/randomnumber/MainActivity.java
@@ -3,16 +3,53 @@ package com.example.randomnumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.randomnumber.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-  private ActivityMainBinding binder;
+    private ActivityMainBinding binder;
+    private List<Integer> Numbers = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binder=ActivityMainBinding.inflate(getLayoutInflater());
+        binder = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binder.getRoot());
+
+        binder.randomFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int random = new Random().nextInt();
+                Numbers.add(random);
+            }
+        });
+        binder.pushFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int i = 0; i < Numbers.size(); i++) {
+
+                    String z = binder.numbers.getText().toString()
+                            + Numbers.get(i).toString()
+                            + ", ";
+
+                    binder.numbers.setText(z);
+                }
+            }
+        });
+
+        binder.clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Numbers = new ArrayList<>();
+                binder.numbers.setText("");
+            }
+        });
+
 
     }
 }
